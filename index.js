0160021a33805d94b40b7aabd5cb6b53b449fbcb
IvanLomaka <ivan.lomaka5@gmail.com>
2022-12-10 11:41:42 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+const reactBuild = path.join(__dirname, "front", "build")
+app.use(express.static(reactBuild))
+
+app.get("/api", async(req, res) => {
+    res.json({"users": ["userOne", "userTwo", "userThree"]})
+})
+
+app.get("*", async(req, res) => {
+    res.sendFile(path.join(reactBuild, 'index.html'))
+})
+
+
+app.listen(PORT, () => console.log('server running at ' + PORT))
